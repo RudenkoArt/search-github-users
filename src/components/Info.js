4b6a349a060f1cdd90a319516f1ad5cc77db2bf6
RudenkoArt <rudnkoart@gmail.com>
2023-02-13 09:41:18 +0200

    add context

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,7 +5,8 @@ import { GoRepo, GoGist } from 'react-icons/go';
 import { FiUsers, FiUserPlus } from 'react-icons/fi';
 
 const UserInfo = () => {
-  return <h2>user info component</h2>;
+  const value = React.useContext(GithubContext);
+  return <h2>user info component : {value}</h2>;
 };
 
 const Wrapper = styled.section`
