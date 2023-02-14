34218d9cb09602174218d754ea3e5576f946038f
RudenkoArt <rudnkoart@gmail.com>
2023-02-14 12:11:34 +0200

    implement Auth0 authentication

--- src/index.js
+++ src/index.js
@@ -5,14 +5,23 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+// dev-kemsp7apehdhfnaf.us.auth0.com
+// lSuhKWVsi0wbcFvx7WU2PewETigSHcms
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
-  </React.StrictMode>
+  <Auth0Provider
+    domain='dev-kemsp7apehdhfnaf.us.auth0.com'
+    clientId='lSuhKWVsi0wbcFvx7WU2PewETigSHcms'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <React.StrictMode>
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
